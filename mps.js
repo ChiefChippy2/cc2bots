5e7e954c0491be5783e2f7d34cae7559b84f63fb
ChiefChippy2 <45596322+ChiefChippy2@users.noreply.github.com>
2020-04-25 15:45:11 +0200

    Create mps.js

---
+++ mps.js
@@ -0,0 +1,8 @@
+async function bSta(){
+let a = (new Date()).getTime()
+Promise.race([new Promise((resolve) => {setTimeout(resolve,3000)}),new Promise((re,rj)=>{fetch("https://tangy-bold-elderberry.glitch.me").then(r=>r.text()).then(r=>{if(r==="OK") re;else rj;})})]).then(k=>{document.querySelector("#bSa").innerHTML="Bot is UP, response time : " + String((new Date()).getTime()-a)+"ms."}).catch(e=>document.querySelector("#bSa").innerHTML="Bot is currently down, maybe check your internet connection?")
+
+            
+
+
+} 
