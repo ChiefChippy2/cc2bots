4d9cd460efa9d2ba34f5b44d2bb2c03ae1d92322
ChiefChippy2 <45596322+ChiefChippy2@users.noreply.github.com>
2020-04-25 16:14:52 +0200

    Update mps.js

--- mps.js
+++ mps.js
@@ -1,6 +1,7 @@
 async function bSta(){
-let a = (new Date()).getTime()
-Promise.race([new Promise((resolve) => {setTimeout(resolve,3000)}),new Promise((re,rj)=>{fetch("https://tangy-bold-elderberry.glitch.me").then(r=>r.text()).then(r=>{if(r==="OK") re;else rj;})})]).then(k=>{document.querySelector("#bSa").innerHTML="Bot is UP, response time : " + String((new Date()).getTime()-a)+"ms."}).catch(e=>document.querySelector("#bSa").innerHTML="Bot is currently down, maybe check your internet connection?")
+let a;
+let b;
+Promise.race([new Promise((resolve) => {setTimeout(resolve,3000)}),new Promise((re,rj)=>{ a = (new Date()).getTime();fetch("https://tangy-bold-elderberry.glitch.me").then(r=>r.text()).then(r=>{b=(new Date()).getTime();if(r==="OK") re;else rj;})})]).then(k=>{document.querySelector("#bSa").innerHTML="Bot is UP, response time : " + String(b-a)+"ms."}).catch(e=>document.querySelector("#bSa").innerHTML="Bot is currently down, maybe check your internet connection?")
 
             
 
