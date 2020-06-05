cb453e281d3a98c12e06fb8c910558cbb4d131da
ChiefChippy2 <45596322+ChiefChippy2@users.noreply.github.com>
2020-06-05 11:19:30 +0200

    Update mps.js

--- mps.js
+++ mps.js
@@ -6,10 +6,7 @@ Promise.race([new Promise((resolve,rej) => {
 new Promise((re,rj)=>{
  a = (new Date()).getTime();
  fetch("https://tangy-bold-elderberry.glitch.me",{mode:"no-cors"}).then(r=>r.text()).then(r=>{
- if(r==="OK") re(new Date().getTime());else rj;})})
- ]).then(k=>{document.querySelector("#bSa").innerHTML=("Bot is UP, response time : "+String(k-a) +" ms.")} ).catch(e=>document.querySelector("#bSa").innerHTML="Bot is currently down, maybe check your internet connection?")
-
-            
-
-
+ re(new Date().getTime())})})
+ ]).then(k=>{document.querySelector("#bSa").innerHTML=("Bot is UP, response time : "+String(k-a) +" ms.")} )
+            .catch(e=>document.querySelector("#bSa").innerHTML="Bot is currently down, maybe check your internet connection?")
 } 
