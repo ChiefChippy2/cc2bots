55d84de08af6cdc99bd97445262f9a492492942a
ChiefChippy2 <45596322+ChiefChippy2@users.noreply.github.com>
2020-06-05 11:16:10 +0200

    Update mps.js

--- mps.js
+++ mps.js
@@ -5,7 +5,7 @@ Promise.race([new Promise((resolve) => {
             setTimeout(resolve,3000)}),
 new Promise((re,rj)=>{
  a = (new Date()).getTime();
- fetch("https://tangy-bold-elderberry.glitch.me").then(r=>r.text()).then(r=>{
+ fetch("https://tangy-bold-elderberry.glitch.me",{mode:"no-cors"}).then(r=>r.text()).then(r=>{
  if(r==="OK") re(new Date().getTime());else rj;})})
  ]).then(k=>{document.querySelector("#bSa").innerHTML=("Bot is UP, response time : "+String(k-a) +" ms.")} ).catch(e=>document.querySelector("#bSa").innerHTML="Bot is currently down, maybe check your internet connection?")
 
