bc45c13ca8912153d3050abe827930e2c8673861
ChiefChippy2 <45596322+ChiefChippy2@users.noreply.github.com>
2020-07-24 15:34:22 +0200

    Update load.js

--- mpleaderboard/load.js
+++ mpleaderboard/load.js
@@ -1,18 +1,17 @@
-//cookie
 const needed=["https://combinatronics.com/ChiefChippy2/cc2bots/master/mpleaderboard/alias.json","https://combinatronics.com/ChiefChippy2/cc2bots/master/mpleaderboard/lbs.json"]
-/* load everything and warn user */
+/* Load everything and warn user */
 Promise.all(
 needed
 .map(x=>fetch(x)
 .then(r=>r.json())
 .then(re=>res(window[x.split("/").reverse()[0].split(".")[0]]=re))
-.catch(e=>if(prompt("Uh oh, something went wrong whilst trying to load one of all of the resources. Retry now?")) window.reload())
-)
+.catch(e=>{if(prompt("Uh oh, something went wrong whilst trying to load one of all of the resources. Retry now?")) window.reload()
+          })))
 .then(e=>window.emit("AllLoaded"))
 //Tmw = timeout warning ( to prompt for reload if it takes too long )
 window.tmw=window.setTimeout(
-()=>if(prompt("Uh oh, something went wrong whilst trying to load one of all of the resources. Retry now?")) window.reload()
-,10*1000)
+()=>{if(prompt("Uh oh, something went wrong whilst trying to load one of all of the resources. Retry now?")) window.reload()
+    },10*1000)
 
 window.on("AllLoaded",()=>{
  //We know all stuff has been loaded, now setup fuse
@@ -22,10 +21,10 @@ window.on("AllLoaded",()=>{
 }
 
 window.lbsearch = new Fuse(Object.keys(window.lbs), options)
-//now show the body div
+//Now show the body div
 document.querySelector(".bod").style.display="yes";
 document.querySelector(".loading").style.display="none";
-//emit ready
-windiw.emit("ready")
+//Emit ready
+window.emit("ready")
 })
   
