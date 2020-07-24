8b0088111b21168c14116b32ebd8e8a8601b4923
ChiefChippy2 <45596322+ChiefChippy2@users.noreply.github.com>
2020-07-24 15:19:16 +0200

    Update load.js

--- mpleaderboard/load.js
+++ mpleaderboard/load.js
@@ -1,5 +1,4 @@
 //cookie
-document.body.style.backgroundColor=document.cookie.search("darkM=false")!=-1?"white":"#44464a";
 const needed=["https://combinatronics.com/ChiefChippy2/cc2bots/master/mpleaderboard/alias.json","https://combinatronics.com/ChiefChippy2/cc2bots/master/mpleaderboard/lbs.json"]
 /* load everything and warn user */
 Promise.all(
@@ -14,3 +13,18 @@ needed
 window.tmw=window.setTimeout(
 ()=>if(prompt("Uh oh, something went wrong whilst trying to load one of all of the resources. Retry now?")) window.reload()
 ,10*1000)
+
+window.on("AllLoaded",()=>{
+ //We know all stuff has been loaded, now setup fuse
+  const options = {
+  includeScore: true,
+    ignoreLocation:true
+}
+
+window.lbsearch = new Fuse(Object.keys(window.lbs), options)
+//now show the body div
+document.querySelector(".bod").style.display="yes";
+//emit ready
+windiw.emit("ready")
+})
+  
