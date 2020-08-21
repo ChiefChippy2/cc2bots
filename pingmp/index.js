e40563777a145287bf5fa14d67eddf61955eff69
ChiefChippy2 <45596322+ChiefChippy2@users.noreply.github.com>
2020-08-21 23:56:27 +0200

    Update index.js

--- pingmp/index.js
+++ pingmp/index.js
@@ -1,19 +1,20 @@
 window.addEventListener("DOMContentLoaded",start)
 
 function start(n,excludes){
-let lon=["eu","us","clans"]
-let poa=lon.filter(x=>!excludes.includes(x)).map(x=>fetch("https://dns.google.com/resolve?name="+x+".mineplex.com&type=a"))
+let lon=["us","clans"]
+let poa=lon.filter(x=>!excludes.includes(x)).map(y=>fetch("https://dns.google.com/resolve?name="+y+".mineplex.com&type=a"))
 Promise.all(poa).then(ress=>{
 Promise.all(ress.filter(x=>x.ok||x.status===200).map(x=>x.json())).then(allips=>{
 const ip=allips.filter(x=>x.Answer&&x.Answer[0].data).map(x=>x.Answer[0].data).map(x=>x).flat()
-displayIP(ip)
+displayIP(ip);
 (async function(){
 for(let s of ip){
 let results=[];
 for(let i=1;i<n;i++){
-results.push(await ping(s))
+let pin=await ping(s)
+results.push(pin)
 }
-displayPing(s,results.reduce((a,c)=>a+=c)/n)
+displayPing(s,results.reduce((a,c)=>a+c)/n)
 }
 })()
 
